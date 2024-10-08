56a0556413542024d206ee782d40fb53ee5f4ce6
jth <juthomas@tandemdiabetes.com>
2024-10-08 17:05:20 +0200

    main.py: Use extrem example

--- benchmark_string_filter/main.py
+++ benchmark_string_filter/main.py
@@ -1,7 +1,8 @@
 from benchmark_string_filter.default import detect_special_characters
+from benchmark_string_filter.samples import *
 
 def main() -> None:
-    (is_valid, e) = detect_special_characters("Hello, World!")
+    (is_valid, e) = detect_special_characters(lorem_ipsum_100000_valid)
     print(f'{is_valid} - "{e}"')
 
 
