c6c1ef1d970fa9914572c792c9fdf8470b2230a0
jth <juthomas@tandemdiabetes.com>
2024-10-08 15:22:26 +0200

    default.py: Add default reference implementation

---
+++ benchmark_string_filter/default.py
@@ -0,0 +1,12 @@
+import typing
+
+
+def detect_special_characters(
+    input_str: str,
+) -> typing.Tuple[bool, typing.Optional[str]]:
+    """Check if input string contains any characters not in the whitelist."""
+    whitelist = set("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    for char in input_str:
+        if char not in whitelist:
+            return True, char
+    return False, None
