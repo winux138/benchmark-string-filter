d45ab051b4d36f285925b0e322bed00470a8f17b
jth <juthomas@tandemdiabetes.com>
2024-10-08 16:40:57 +0200

    main.py: Add basic usage

--- benchmark_string_filter/main.py
+++ benchmark_string_filter/main.py
@@ -1,5 +1,8 @@
+from benchmark_string_filter.default import detect_special_characters
+
 def main() -> None:
-    print("Hello, World!")
+    (is_valid, e) = detect_special_characters("Hello, World!")
+    print(f'{is_valid} - "{e}"')
 
 
 if __name__ == "__main__":
